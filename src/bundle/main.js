9d82fa1c1fe558686f918b7aa8c341db5fdf93cd
dmayerdesign <d.a.mayer92@gmail.com>
2016-10-16 02:28:52 -0400

    changed 'join the community' to 'join'

--- src/bundle/main.js
+++ src/bundle/main.js
@@ -37901,14 +37901,14 @@
 	var create_post_component_1 = __webpack_require__(89);
 	var search_box_component_1 = __webpack_require__(90);
 	var contact_component_1 = __webpack_require__(91);
-	var form_field_component_1 = __webpack_require__(355);
+	var form_field_component_1 = __webpack_require__(92);
 	var user_service_1 = __webpack_require__(68);
 	var search_service_1 = __webpack_require__(76);
-	var ng2_click_outside_1 = __webpack_require__(92);
+	var ng2_click_outside_1 = __webpack_require__(93);
 	var categories_service_1 = __webpack_require__(79);
 	var org_service_1 = __webpack_require__(75);
 	var app_service_1 = __webpack_require__(70);
-	var truncate_pipe_1 = __webpack_require__(94);
+	var truncate_pipe_1 = __webpack_require__(95);
 	var core_2 = __webpack_require__(3);
 	core_2.enableProdMode();
 	var routing = router_1.RouterModule.forRoot([
@@ -54293,417 +54293,6 @@
 	    if (typeof Reflect === "object" && typeof Reflect.metadata === "function") return Reflect.metadata(k, v);
 	};
 	var core_1 = __webpack_require__(3);
-	var click_outside_directive_1 = __webpack_require__(93);
-	exports.ClickOutsideDirective = click_outside_directive_1.default;
-	var ClickOutsideModule = (function () {
-	    function ClickOutsideModule() {
-	    }
-	    ClickOutsideModule = __decorate([
-	        core_1.NgModule({
-	            declarations: [click_outside_directive_1.default],
-	            exports: [click_outside_directive_1.default]
-	        }), 
-	        __metadata('design:paramtypes', [])
-	    ], ClickOutsideModule);
-	    return ClickOutsideModule;
-	}());
-	exports.ClickOutsideModule = ClickOutsideModule;
-
-
-/***/ },
-/* 93 */
-/***/ function(module, exports, __webpack_require__) {
-
-	"use strict";
-	var __decorate = (this && this.__decorate) || function (decorators, target, key, desc) {
-	    var c = arguments.length, r = c < 3 ? target : desc === null ? desc = Object.getOwnPropertyDescriptor(target, key) : desc, d;
-	    if (typeof Reflect === "object" && typeof Reflect.decorate === "function") r = Reflect.decorate(decorators, target, key, desc);
-	    else for (var i = decorators.length - 1; i >= 0; i--) if (d = decorators[i]) r = (c < 3 ? d(r) : c > 3 ? d(target, key, r) : d(target, key)) || r;
-	    return c > 3 && r && Object.defineProperty(target, key, r), r;
-	};
-	var __metadata = (this && this.__metadata) || function (k, v) {
-	    if (typeof Reflect === "object" && typeof Reflect.metadata === "function") return Reflect.metadata(k, v);
-	};
-	var __param = (this && this.__param) || function (paramIndex, decorator) {
-	    return function (target, key) { decorator(target, key, paramIndex); }
-	};
-	var core_1 = __webpack_require__(3);
-	var platform_browser_1 = __webpack_require__(22);
-	var ClickOutsideDirective = (function () {
-	    function ClickOutsideDirective(_document, _el) {
-	        this._document = _document;
-	        this._el = _el;
-	        this.attachOutsideOnClick = false;
-	        this.clickOutside = new core_1.EventEmitter();
-	        this._initOnClickBody = this._initOnClickBody.bind(this);
-	        this._onClickBody = this._onClickBody.bind(this);
-	    }
-	    ClickOutsideDirective.prototype.ngOnInit = function () {
-	        this._init();
-	    };
-	    ClickOutsideDirective.prototype.ngOnDestroy = function () {
-	        if (this.attachOutsideOnClick) {
-	            this._el.nativeElement.removeEventListener('click', this._initOnClickBody);
-	        }
-	        this._document.body.removeEventListener('click', this._onClickBody);
-	    };
-	    ClickOutsideDirective.prototype.ngOnChanges = function (changes) {
-	        if (changes['attachOutsideOnClick'] &&
-	            changes['attachOutsideOnClick'].previousValue !== changes['attachOutsideOnClick'].currentValue) {
-	            this._init();
-	        }
-	    };
-	    ClickOutsideDirective.prototype._init = function () {
-	        if (this.attachOutsideOnClick) {
-	            this._el.nativeElement.addEventListener('click', this._initOnClickBody);
-	        }
-	        else {
-	            this._initOnClickBody();
-	        }
-	    };
-	    ClickOutsideDirective.prototype._initOnClickBody = function () {
-	        this._document.body.addEventListener('click', this._onClickBody);
-	    };
-	    ClickOutsideDirective.prototype._onClickBody = function (e) {
-	        if (!this._el.nativeElement.contains(e.target)) {
-	            this.clickOutside.emit(e);
-	            if (this.attachOutsideOnClick) {
-	                this._document.body.removeEventListener('click', this._onClickBody);
-	            }
-	        }
-	    };
-	    __decorate([
-	        core_1.Input(), 
-	        __metadata('design:type', Boolean)
-	    ], ClickOutsideDirective.prototype, "attachOutsideOnClick", void 0);
-	    __decorate([
-	        core_1.Output(), 
-	        __metadata('design:type', core_1.EventEmitter)
-	    ], ClickOutsideDirective.prototype, "clickOutside", void 0);
-	    ClickOutsideDirective = __decorate([
-	        core_1.Directive({ selector: '[clickOutside]' }),
-	        __param(0, core_1.Inject(platform_browser_1.DOCUMENT)), 
-	        __metadata('design:paramtypes', [HTMLDocument, core_1.ElementRef])
-	    ], ClickOutsideDirective);
-	    return ClickOutsideDirective;
-	}());
-	Object.defineProperty(exports, "__esModule", { value: true });
-	exports.default = ClickOutsideDirective;
-
-
-/***/ },
-/* 94 */
-/***/ function(module, exports, __webpack_require__) {
-
-	"use strict";
-	var __decorate = (this && this.__decorate) || function (decorators, target, key, desc) {
-	    var c = arguments.length, r = c < 3 ? target : desc === null ? desc = Object.getOwnPropertyDescriptor(target, key) : desc, d;
-	    if (typeof Reflect === "object" && typeof Reflect.decorate === "function") r = Reflect.decorate(decorators, target, key, desc);
-	    else for (var i = decorators.length - 1; i >= 0; i--) if (d = decorators[i]) r = (c < 3 ? d(r) : c > 3 ? d(target, key, r) : d(target, key)) || r;
-	    return c > 3 && r && Object.defineProperty(target, key, r), r;
-	};
-	var __metadata = (this && this.__metadata) || function (k, v) {
-	    if (typeof Reflect === "object" && typeof Reflect.metadata === "function") return Reflect.metadata(k, v);
-	};
-	var core_1 = __webpack_require__(3);
-	var TruncatePipe = (function () {
-	    function TruncatePipe() {
-	    }
-	    TruncatePipe.prototype.transform = function (value, arg1, arg2) {
-	        if (value && arg1 !== 0) {
-	            var limit = arg1 ? parseInt(arg1, 10) : 200;
-	            var trail = arg2 || '...';
-	            return value.length > limit ? value.substring(0, limit) + trail : value;
-	        }
-	        else
-	            return value;
-	    };
-	    TruncatePipe = __decorate([
-	        core_1.Pipe({
-	            name: 'truncate'
-	        }), 
-	        __metadata('design:paramtypes', [])
-	    ], TruncatePipe);
-	    return TruncatePipe;
-	}());
-	exports.TruncatePipe = TruncatePipe;
-
-
-/***/ },
-/* 95 */,
-/* 96 */,
-/* 97 */,
-/* 98 */,
-/* 99 */,
-/* 100 */,
-/* 101 */,
-/* 102 */,
-/* 103 */,
-/* 104 */,
-/* 105 */,
-/* 106 */,
-/* 107 */,
-/* 108 */,
-/* 109 */,
-/* 110 */,
-/* 111 */,
-/* 112 */,
-/* 113 */,
-/* 114 */,
-/* 115 */,
-/* 116 */,
-/* 117 */,
-/* 118 */,
-/* 119 */,
-/* 120 */,
-/* 121 */,
-/* 122 */,
-/* 123 */,
-/* 124 */,
-/* 125 */,
-/* 126 */,
-/* 127 */,
-/* 128 */,
-/* 129 */,
-/* 130 */,
-/* 131 */,
-/* 132 */,
-/* 133 */,
-/* 134 */,
-/* 135 */,
-/* 136 */,
-/* 137 */,
-/* 138 */,
-/* 139 */,
-/* 140 */,
-/* 141 */,
-/* 142 */,
-/* 143 */,
-/* 144 */,
-/* 145 */,
-/* 146 */,
-/* 147 */,
-/* 148 */,
-/* 149 */,
-/* 150 */,
-/* 151 */,
-/* 152 */,
-/* 153 */,
-/* 154 */,
-/* 155 */,
-/* 156 */,
-/* 157 */,
-/* 158 */,
-/* 159 */,
-/* 160 */,
-/* 161 */,
-/* 162 */,
-/* 163 */,
-/* 164 */,
-/* 165 */,
-/* 166 */,
-/* 167 */,
-/* 168 */,
-/* 169 */,
-/* 170 */,
-/* 171 */,
-/* 172 */,
-/* 173 */,
-/* 174 */,
-/* 175 */,
-/* 176 */,
-/* 177 */,
-/* 178 */,
-/* 179 */,
-/* 180 */,
-/* 181 */,
-/* 182 */,
-/* 183 */,
-/* 184 */,
-/* 185 */,
-/* 186 */,
-/* 187 */,
-/* 188 */,
-/* 189 */,
-/* 190 */,
-/* 191 */,
-/* 192 */,
-/* 193 */,
-/* 194 */,
-/* 195 */,
-/* 196 */,
-/* 197 */,
-/* 198 */,
-/* 199 */,
-/* 200 */,
-/* 201 */,
-/* 202 */,
-/* 203 */,
-/* 204 */,
-/* 205 */,
-/* 206 */,
-/* 207 */,
-/* 208 */,
-/* 209 */,
-/* 210 */,
-/* 211 */,
-/* 212 */,
-/* 213 */,
-/* 214 */,
-/* 215 */,
-/* 216 */,
-/* 217 */,
-/* 218 */,
-/* 219 */,
-/* 220 */,
-/* 221 */,
-/* 222 */,
-/* 223 */,
-/* 224 */,
-/* 225 */,
-/* 226 */,
-/* 227 */,
-/* 228 */,
-/* 229 */,
-/* 230 */,
-/* 231 */,
-/* 232 */,
-/* 233 */,
-/* 234 */,
-/* 235 */,
-/* 236 */,
-/* 237 */,
-/* 238 */,
-/* 239 */,
-/* 240 */,
-/* 241 */,
-/* 242 */,
-/* 243 */,
-/* 244 */,
-/* 245 */,
-/* 246 */,
-/* 247 */,
-/* 248 */,
-/* 249 */,
-/* 250 */,
-/* 251 */,
-/* 252 */,
-/* 253 */,
-/* 254 */,
-/* 255 */,
-/* 256 */,
-/* 257 */,
-/* 258 */,
-/* 259 */,
-/* 260 */,
-/* 261 */,
-/* 262 */,
-/* 263 */,
-/* 264 */,
-/* 265 */,
-/* 266 */,
-/* 267 */,
-/* 268 */,
-/* 269 */,
-/* 270 */,
-/* 271 */,
-/* 272 */,
-/* 273 */,
-/* 274 */,
-/* 275 */,
-/* 276 */,
-/* 277 */,
-/* 278 */,
-/* 279 */,
-/* 280 */,
-/* 281 */,
-/* 282 */,
-/* 283 */,
-/* 284 */,
-/* 285 */,
-/* 286 */,
-/* 287 */,
-/* 288 */,
-/* 289 */,
-/* 290 */,
-/* 291 */,
-/* 292 */,
-/* 293 */,
-/* 294 */,
-/* 295 */,
-/* 296 */,
-/* 297 */,
-/* 298 */,
-/* 299 */,
-/* 300 */,
-/* 301 */,
-/* 302 */,
-/* 303 */,
-/* 304 */,
-/* 305 */,
-/* 306 */,
-/* 307 */,
-/* 308 */,
-/* 309 */,
-/* 310 */,
-/* 311 */,
-/* 312 */,
-/* 313 */,
-/* 314 */,
-/* 315 */,
-/* 316 */,
-/* 317 */,
-/* 318 */,
-/* 319 */,
-/* 320 */,
-/* 321 */,
-/* 322 */,
-/* 323 */,
-/* 324 */,
-/* 325 */,
-/* 326 */,
-/* 327 */,
-/* 328 */,
-/* 329 */,
-/* 330 */,
-/* 331 */,
-/* 332 */,
-/* 333 */,
-/* 334 */,
-/* 335 */,
-/* 336 */,
-/* 337 */,
-/* 338 */,
-/* 339 */,
-/* 340 */,
-/* 341 */,
-/* 342 */,
-/* 343 */,
-/* 344 */,
-/* 345 */,
-/* 346 */,
-/* 347 */,
-/* 348 */,
-/* 349 */,
-/* 350 */,
-/* 351 */,
-/* 352 */,
-/* 353 */,
-/* 354 */,
-/* 355 */
-/***/ function(module, exports, __webpack_require__) {
-
-	"use strict";
-	var __decorate = (this && this.__decorate) || function (decorators, target, key, desc) {
-	    var c = arguments.length, r = c < 3 ? target : desc === null ? desc = Object.getOwnPropertyDescriptor(target, key) : desc, d;
-	    if (typeof Reflect === "object" && typeof Reflect.decorate === "function") r = Reflect.decorate(decorators, target, key, desc);
-	    else for (var i = decorators.length - 1; i >= 0; i--) if (d = decorators[i]) r = (c < 3 ? d(r) : c > 3 ? d(target, key, r) : d(target, key)) || r;
-	    return c > 3 && r && Object.defineProperty(target, key, r), r;
-	};
-	var __metadata = (this && this.__metadata) || function (k, v) {
-	    if (typeof Reflect === "object" && typeof Reflect.metadata === "function") return Reflect.metadata(k, v);
-	};
-	var core_1 = __webpack_require__(3);
 	var categories_service_1 = __webpack_require__(79);
 	var app_service_1 = __webpack_require__(70);
 	var FormFieldComponent = (function () {
@@ -54792,6 +54381,157 @@
 	    return FormFieldComponent;
 	}());
 	exports.FormFieldComponent = FormFieldComponent;
+
+
+/***/ },
+/* 93 */
+/***/ function(module, exports, __webpack_require__) {
+
+	"use strict";
+	var __decorate = (this && this.__decorate) || function (decorators, target, key, desc) {
+	    var c = arguments.length, r = c < 3 ? target : desc === null ? desc = Object.getOwnPropertyDescriptor(target, key) : desc, d;
+	    if (typeof Reflect === "object" && typeof Reflect.decorate === "function") r = Reflect.decorate(decorators, target, key, desc);
+	    else for (var i = decorators.length - 1; i >= 0; i--) if (d = decorators[i]) r = (c < 3 ? d(r) : c > 3 ? d(target, key, r) : d(target, key)) || r;
+	    return c > 3 && r && Object.defineProperty(target, key, r), r;
+	};
+	var __metadata = (this && this.__metadata) || function (k, v) {
+	    if (typeof Reflect === "object" && typeof Reflect.metadata === "function") return Reflect.metadata(k, v);
+	};
+	var core_1 = __webpack_require__(3);
+	var click_outside_directive_1 = __webpack_require__(94);
+	exports.ClickOutsideDirective = click_outside_directive_1.default;
+	var ClickOutsideModule = (function () {
+	    function ClickOutsideModule() {
+	    }
+	    ClickOutsideModule = __decorate([
+	        core_1.NgModule({
+	            declarations: [click_outside_directive_1.default],
+	            exports: [click_outside_directive_1.default]
+	        }), 
+	        __metadata('design:paramtypes', [])
+	    ], ClickOutsideModule);
+	    return ClickOutsideModule;
+	}());
+	exports.ClickOutsideModule = ClickOutsideModule;
+
+
+/***/ },
+/* 94 */
+/***/ function(module, exports, __webpack_require__) {
+
+	"use strict";
+	var __decorate = (this && this.__decorate) || function (decorators, target, key, desc) {
+	    var c = arguments.length, r = c < 3 ? target : desc === null ? desc = Object.getOwnPropertyDescriptor(target, key) : desc, d;
+	    if (typeof Reflect === "object" && typeof Reflect.decorate === "function") r = Reflect.decorate(decorators, target, key, desc);
+	    else for (var i = decorators.length - 1; i >= 0; i--) if (d = decorators[i]) r = (c < 3 ? d(r) : c > 3 ? d(target, key, r) : d(target, key)) || r;
+	    return c > 3 && r && Object.defineProperty(target, key, r), r;
+	};
+	var __metadata = (this && this.__metadata) || function (k, v) {
+	    if (typeof Reflect === "object" && typeof Reflect.metadata === "function") return Reflect.metadata(k, v);
+	};
+	var __param = (this && this.__param) || function (paramIndex, decorator) {
+	    return function (target, key) { decorator(target, key, paramIndex); }
+	};
+	var core_1 = __webpack_require__(3);
+	var platform_browser_1 = __webpack_require__(22);
+	var ClickOutsideDirective = (function () {
+	    function ClickOutsideDirective(_document, _el) {
+	        this._document = _document;
+	        this._el = _el;
+	        this.attachOutsideOnClick = false;
+	        this.clickOutside = new core_1.EventEmitter();
+	        this._initOnClickBody = this._initOnClickBody.bind(this);
+	        this._onClickBody = this._onClickBody.bind(this);
+	    }
+	    ClickOutsideDirective.prototype.ngOnInit = function () {
+	        this._init();
+	    };
+	    ClickOutsideDirective.prototype.ngOnDestroy = function () {
+	        if (this.attachOutsideOnClick) {
+	            this._el.nativeElement.removeEventListener('click', this._initOnClickBody);
+	        }
+	        this._document.body.removeEventListener('click', this._onClickBody);
+	    };
+	    ClickOutsideDirective.prototype.ngOnChanges = function (changes) {
+	        if (changes['attachOutsideOnClick'] &&
+	            changes['attachOutsideOnClick'].previousValue !== changes['attachOutsideOnClick'].currentValue) {
+	            this._init();
+	        }
+	    };
+	    ClickOutsideDirective.prototype._init = function () {
+	        if (this.attachOutsideOnClick) {
+	            this._el.nativeElement.addEventListener('click', this._initOnClickBody);
+	        }
+	        else {
+	            this._initOnClickBody();
+	        }
+	    };
+	    ClickOutsideDirective.prototype._initOnClickBody = function () {
+	        this._document.body.addEventListener('click', this._onClickBody);
+	    };
+	    ClickOutsideDirective.prototype._onClickBody = function (e) {
+	        if (!this._el.nativeElement.contains(e.target)) {
+	            this.clickOutside.emit(e);
+	            if (this.attachOutsideOnClick) {
+	                this._document.body.removeEventListener('click', this._onClickBody);
+	            }
+	        }
+	    };
+	    __decorate([
+	        core_1.Input(), 
+	        __metadata('design:type', Boolean)
+	    ], ClickOutsideDirective.prototype, "attachOutsideOnClick", void 0);
+	    __decorate([
+	        core_1.Output(), 
+	        __metadata('design:type', core_1.EventEmitter)
+	    ], ClickOutsideDirective.prototype, "clickOutside", void 0);
+	    ClickOutsideDirective = __decorate([
+	        core_1.Directive({ selector: '[clickOutside]' }),
+	        __param(0, core_1.Inject(platform_browser_1.DOCUMENT)), 
+	        __metadata('design:paramtypes', [HTMLDocument, core_1.ElementRef])
+	    ], ClickOutsideDirective);
+	    return ClickOutsideDirective;
+	}());
+	Object.defineProperty(exports, "__esModule", { value: true });
+	exports.default = ClickOutsideDirective;
+
+
+/***/ },
+/* 95 */
+/***/ function(module, exports, __webpack_require__) {
+
+	"use strict";
+	var __decorate = (this && this.__decorate) || function (decorators, target, key, desc) {
+	    var c = arguments.length, r = c < 3 ? target : desc === null ? desc = Object.getOwnPropertyDescriptor(target, key) : desc, d;
+	    if (typeof Reflect === "object" && typeof Reflect.decorate === "function") r = Reflect.decorate(decorators, target, key, desc);
+	    else for (var i = decorators.length - 1; i >= 0; i--) if (d = decorators[i]) r = (c < 3 ? d(r) : c > 3 ? d(target, key, r) : d(target, key)) || r;
+	    return c > 3 && r && Object.defineProperty(target, key, r), r;
+	};
+	var __metadata = (this && this.__metadata) || function (k, v) {
+	    if (typeof Reflect === "object" && typeof Reflect.metadata === "function") return Reflect.metadata(k, v);
+	};
+	var core_1 = __webpack_require__(3);
+	var TruncatePipe = (function () {
+	    function TruncatePipe() {
+	    }
+	    TruncatePipe.prototype.transform = function (value, arg1, arg2) {
+	        if (value && arg1 !== 0) {
+	            var limit = arg1 ? parseInt(arg1, 10) : 200;
+	            var trail = arg2 || '...';
+	            return value.length > limit ? value.substring(0, limit) + trail : value;
+	        }
+	        else
+	            return value;
+	    };
+	    TruncatePipe = __decorate([
+	        core_1.Pipe({
+	            name: 'truncate'
+	        }), 
+	        __metadata('design:paramtypes', [])
+	    ], TruncatePipe);
+	    return TruncatePipe;
+	}());
+	exports.TruncatePipe = TruncatePipe;
 
 
 /***/ }
